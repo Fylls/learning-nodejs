df693cb590b7880b7bfb96d1afc8fd971b3316da
Fylls <sergentifilippo@gmail.com>
2020-08-17 14:03:58 +0200

    express full

--- #3 Express/_bodyparser.js
+++ #3 Express/_bodyparser.js
@@ -1,12 +1,17 @@
-// parsing the body with BP
 const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
 
+// parsing the body with BP
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// or
 
 // parsing the body with Express
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// express removed and readded and removed and finally readded BP from express.js library
+// BP library won't change, however express.js can, so it is recommended to install body-parser and express separately
--- #3 Express/server/app.js
+++ #3 Express/server/app.js
@@ -10,6 +10,8 @@ const app = express()
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// serving static files
 app.use(express.static(path.join(__dirname, "public")))
 
 // Routes defs
@@ -20,7 +22,7 @@ const shopRoutes = require("./routes/shop")
 app.use("/admin", adminRoutes)
 app.use(shopRoutes)
 
-// serving static file
+// 404 page not found
 app.use((req, res) =>
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
 )
