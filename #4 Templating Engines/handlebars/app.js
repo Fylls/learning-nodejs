5c2a8a1daab80be26933b9ea3e16f30d29921f2f
Fylls <sergentifilippo@gmail.com>
2020-08-18 11:55:40 +0200

    handlebars
    86

--- #4 Templating Engines/handlebars/app.js
+++ #4 Templating Engines/handlebars/app.js
@@ -4,9 +4,23 @@ const path = require("path")
 // dependencies
 const express = require("express")
 const bodyParser = require("body-parser")
+const expressHbs = require("express-handlebars")
 
 // request handler
 const app = express()
+
+// handlebars
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+)
+
+app.set("view engine", "hbs")
+app.set("views", "views")
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -23,9 +37,9 @@ app.use("/admin", adminData.routes)
 app.use(shopRoutes)
 
 // 404 page not found
-app.use((req, res) =>
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
-)
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" })
+})
 
 // starting the server
 const PORT = 3000
